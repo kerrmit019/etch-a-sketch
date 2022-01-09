cd820e6c8fdd8955595c667338dc0b2b5773f34f
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-09 13:03:41 +1300

    Add click event to change grid to blue when clicked

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ for (i = 0; i < 16; i++) {
       "style",
       "border: 1px solid blue; height: 20px; width: 20px"
     );
+    // add class of square
+    square.classList.add("square");
     // square.textContent = (i + 1) * (j + 1);
+
+    // add event listener for click to turn blue (not what we want at end
+    // but good intermediary step)
+    square.addEventListener("click", (e) => {
+      console.log(e.target);
+      e.target.style["background-color"] = "blue";
+    });
     gridContainer.appendChild(square);
   }
 }
 
 // TODO Add event listeners to change colour of grid divs when hover over them
+// const squares = document.querySelectorAll(".square");
+
+// squares.forEach((square) =>
+// );
