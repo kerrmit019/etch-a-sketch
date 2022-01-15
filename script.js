d599c192262f3f610f0c945ee0a25ac951cf2106
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-15 14:01:10 +1300

    Add reset function (clears grid of coloured squares)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
+const resetButton = document.querySelector(".reset-button");
+const numberOfSquaresPerSide = 16;
 
+// set up grid of n X n squares
 function drawGrid(numberOfSquaresPerSide) {
   for (i = 0; i < numberOfSquaresPerSide; i++) {
     for (j = 0; j < numberOfSquaresPerSide; j++) {
@@ -10,6 +13,7 @@ function drawGrid(numberOfSquaresPerSide) {
         "style",
         `height: ${squareLength}px; width: ${squareLength}px`
       );
+      square.classList.add("square");
       // square.textContent = (i + 1) * (j + 1);
 
       // add event listener for pointerenter to turn blue when mouse enters square
@@ -22,6 +26,16 @@ function drawGrid(numberOfSquaresPerSide) {
   }
 }
 
+// clears the grid of coloured squares - turns all squares to white
+function resetGrid() {
+  // select all squares.
+  const squares = document.querySelectorAll(".square");
+  // change each square colour to white to reset
+  squares.forEach((square) => (square.style["background-color"] = "white"));
+}
+
 // draw grid of 16x16
 // drawGrid(16);
-drawGrid(64);
+drawGrid(numberOfSquaresPerSide);
+
+resetButton.addEventListener("click", resetGrid);
